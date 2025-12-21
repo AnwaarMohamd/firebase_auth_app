938e93a45bf7c324a63b8a5b418c45aeef3cc4ac
AnwaarMohamd <anwaarmohmd213@gmail.com>
2025-12-21 05:53:41 +0200

    login ui details

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,12 +12,126 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Screen'),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //image
+              
+              Image.asset(
+                'images/login_image.png',
+                height: 290,
+                width: 300,
+              ),
+              // title
+
+              Text(
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: GoogleFonts.robotoCondensed(
+                    fontWeight: FontWeight.bold,
+                  ).fontFamily,
+                ),
+              ),
+              // subtitle
+              SizedBox(height: 10),
+              Text(
+                'Welcome back! Please login to your account.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
+              ),
+
+              // email textfield
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+
+              // password textfield
+              SizedBox(height: 2),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+
+              // login button
+
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  width: double.infinity,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontFamily: GoogleFonts.robotoCondensed(
+                        ).fontFamily,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              // signup text
+              
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Welcome to the Login Screen!'),
-      ),
+      
     );
   }
 }
